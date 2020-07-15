fa6df4fdc808eead9730405aab088bc4a8f9ed97
paulaschunck <49788988+paulaschunck@users.noreply.github.com>
2020-07-14 21:17:28 -0300

    Aplicação de boas práticas
    Aplicação de boas práticas nos arquivos .

--- mentoriajavascript/javascriptguanabara/exercicio014/script.js
+++ mentoriajavascript/javascriptguanabara/exercicio014/script.js
@@ -1,19 +1,19 @@
-function carregar () {
-    var msg= window.document.getElementbyId('msg')
-    var img= window.document.getElementbyId('imagem')
-    var data= new Date()
-    var hora= data.getHours()
-    msg.innerHTML =`Agora são ${hora} horas.`
-    if (hora >= 0 && hora < 12 )  {
-        BOM DIA!
-        img.src = 'manha.png'
-    } else if (hora >= 12 && hora <= 18) {
-        BOA TARDE!
-        img.src = 'tarde.png'
-    }  else {
-        BOA NOITE!!
-        img.src ='noite.png'
+function load () {    
+  const msg= document.getElementById('msg')
+  let img= document.getElementById('imagem')
+  let data= new Date()
+  let hora= data.getHours()
 
-    }
+  msg.innerHTML =`Agora são ${hora} horas.`
 
+  if (hora >= 0 && hora < 12 )  {
+      // BOM DIA!
+    img.src = 'manha.png'
+  } else if (hora >= 12 && hora <= 18) {
+      //BOA TARDE!
+    img.src = 'tarde.png'
+  }  else {
+      // BOA NOITE!!
+    img.src ='noite.png'
+  }
 }
